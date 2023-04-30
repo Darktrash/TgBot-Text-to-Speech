5fd7bae489113a469eb17eb383f287fe847770cb
Darktrash <69591370+Darktrash@users.noreply.github.com>
2023-04-30 23:13:30 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_TOKEN = '<your token>'
+WELCOME_MESSAGE = '''Send a text message and the bot will return a voice message.
+The language will be detected automatically. Works with 'ru' and 'en'.'''
+DEBUG_MODE = False
+DEBUG_MESSAGE = '''Converting "{}" to speech...
+Detected language: {}'''
